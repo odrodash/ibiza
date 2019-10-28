af0d2a771afb3314b3a890c8f3ddc3fc6bf121c0
Oscar Rodas <oscar.d.rodas@gmail.com>
2019-10-28 17:33:29 -0300

    controller terminado

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :available, presence: true
+  validates :delivery, presence: true
+  validates :price, presence: true
 
+  # validates :cocktail, uniqueness: { scope: :ingredient, message: "only allows letters" }
 end
