9792ebd0bf27d43afda7f7cd3a5197511166f817
Marco Molin <marcomolin45@gmail.com>
2019-10-29 15:39:17 -0300

    terminado views

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,8 @@ ActiveRecord::Schema.define(version: 2019_10_28_205537) do
     t.string "return"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -41,7 +42,6 @@ ActiveRecord::Schema.define(version: 2019_10_28_205537) do
     t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
 end
