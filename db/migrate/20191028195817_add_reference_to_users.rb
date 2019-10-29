5cb2c64a1dcf601f74a837943e53c4986d21e371
Oscar Rodas <oscar.d.rodas@gmail.com>
2019-10-29 10:51:56 -0300

    card de resultados pra el show lista

--- db/migrate/20191028195817_add_reference_to_users.rb
+++ db/migrate/20191028195817_add_reference_to_users.rb
@@ -1,5 +1,5 @@
 class AddReferenceToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_reference :users, :user, foreign_key: true
+    # add_reference :users, :user, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_28_194248) do
+ActiveRecord::Schema.define(version: 2019_10_28_195817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
