6fc0703b1011b1dc2074a80bfbe6272925c409d0
Nicolas <nicolas@elsemillerocoworking.com>
2019-10-30 15:15:37 -0300

    hot fix 1

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,14 @@
 class BookingsController < ApplicationController
+
   def show
     @booking = Booking.find(params[:id])
+    authorize @booking
   end
 
   def new
     @booking = Booking.new
     @product = Product.find(params["product_id"])
+    authorize @booking
   end
 
   def create
@@ -15,5 +18,6 @@ class BookingsController < ApplicationController
     @booking.user = current_user
     @booking.save
     redirect_to booking_path(@booking)
+    authorize @booking
   end
 end
