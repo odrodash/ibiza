96df9a95b8c4e945f9f464c208b36005d1467e1d
Nicolas <nicolas@elsemillerocoworking.com>
2019-10-30 14:01:16 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "Creating users and products"
+2.times do
+  name = Faker::Name.unique.name
+  user = User.create(
+    name: name,
+    last_name: Faker::Artist.name ,
+    email: Faker::Internet.email,
+    address:  Faker::Address.full_address,
+    password: "123456"
+  )
+
+  5.times do
+    puts "creating a new poduct"
+    name = Faker::Commerce::product_name
+    category = Faker::Beer.brand
+    description = "This product is for renting. The category is #{category}"
+    photo = Unsplash::Photo.search("#{name}").first
+    product = Product.new(
+      name: name,
+      category: category,
+      description: description,
+      price: rand(100..1000),
+      location: Faker::Address.full_address,
+      remote_photo_url: photo.urls["regular"]
+    )
+    product.user = user
+    product.save!
+  end
+end
