64d59f1ce2d6d11b400d1ee07503180bd988976d
Oscar Rodas <oscar.d.rodas@gmail.com>
2019-10-31 14:43:36 -0300

    Added booking disable dates

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,8 +9,9 @@ class Product < ApplicationRecord
   # validates :available, presence: true
   # validates :delivery, presence: true
   validates :price, presence: true
+
   DELIVERY = ["Recibirlo (recargo +10%)", "Pasar a buscarlo"]
-  # validates :cocktail, uniqueness: { scope: :ingredient, message: "only allows letters" }
+  CATEGORY = ["Audio", "Decoracion", "Mobiliario", "Juegos", "Luces", "Otros"]
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
@@ -18,4 +19,17 @@ class Product < ApplicationRecord
   def self.results(name, category)
     return Product.where(["name LIKE ? and category = ?", "%#{name}%", category])
   end
+
+  def bookings_dates
+    return bookings.map { |booking| booking.booked_at }
+  end
+
+  # def bookings_available
+  #   dates = []
+  #   self.map do |product|
+  #     dates << product.start
+  #     dates << product.end
+  #   end
+  #   return dates
+  # end
 end
